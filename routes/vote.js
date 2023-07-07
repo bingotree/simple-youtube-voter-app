bed1fcfe006b60627375a72307dd7c4d14e91f71
Brian Duncan <brian@bingotree.com>
2023-07-06 20:47:08 -0700

    Update vote.js

--- routes/vote.js
+++ routes/vote.js
@@ -1,1 +1,9 @@
+var express = require('express');
+var router = express.Router();
 
+/* POST vote */
+router.vote('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
