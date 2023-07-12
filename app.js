af35bbc9c4b1a73cc4216ab8bb790c637fadcc76
Brian Duncan <brian@bingotree.com>
2023-07-12 09:01:33 -0700

    latest stuff

--- app.js
+++ app.js
@@ -24,11 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-app.use('/', indexRouter);
-app.use('/vote', voteRouter);
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   next(createError(404));
 });
